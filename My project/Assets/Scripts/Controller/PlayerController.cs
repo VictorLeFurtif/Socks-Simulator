c4fd03135144a086480e13064430d29c3fa429bf
DELATTRE Alissa <a.delattre@rubika-edu.com>
2025-09-10 14:33:39 +0200

    Basic movement playerController

--- My project/Assets/Scripts/Controller/PlayerController.cs
+++ My project/Assets/Scripts/Controller/PlayerController.cs
@@ -5,11 +5,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 moveDirection = new Vector2();
-    [SerializeField] private int speed = 10;
+    private float radius;
 
+    [SerializeField] private int speed = 100;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private ForceMode2D forceType;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        radius = spriteRenderer.bounds.extents.x;
+    }
     private void FixedUpdate()
     {
-        transform.position += new Vector3(moveDirection.x, 0f, 0f) * Time.fixedDeltaTime * speed;
+        CheckBorderCollision();
+        rb.AddForce(moveDirection * speed, forceType);
+    }
+
+    private void CheckBorderCollision()
+    {
+
+        float camWidth = Camera.main.orthographicSize * Camera.main.aspect;
+        Vector3 posToCam = new Vector3(Mathf.Clamp(transform.position.x, -camWidth + radius, camWidth - radius), transform.position.y, transform.position.z);
+        transform.position = posToCam;
     }
 
     public void Move(InputAction.CallbackContext ctx)
@@ -17,7 +35,11 @@ public class PlayerController : MonoBehaviour
         if (ctx.started)
         {
             moveDirection = ctx.ReadValue<Vector2>();
+            moveDirection = new Vector2(moveDirection.x, 0f);
         }
+
+        if (ctx.canceled)
+            moveDirection = Vector2.zero;
     }
 
     public void Attack()
