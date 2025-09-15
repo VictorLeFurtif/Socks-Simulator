9924147bc71c8bd0cae2a3022773ef95eff75d1e
alissa110088 <delattre.alissa@gmail.com>
2025-09-15 21:52:56 +0200

    flag to count point

--- My project/Assets/Scripts/Manager/EventManager.cs
+++ My project/Assets/Scripts/Manager/EventManager.cs
@@ -6,4 +6,6 @@ public static class EventManager
 {
     public static Action attackInput;
     public static Action<PlayerController> UpdateStunAction;
+    public static Action UpdateFlagP1;
+    public static Action UpdateFlagP2;
 }
