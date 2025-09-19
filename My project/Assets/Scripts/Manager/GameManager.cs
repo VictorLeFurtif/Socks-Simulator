53f9d9a21dd17db55ace3a16c4cbbfe5c3913021
alissa110088 <delattre.alissa@gmail.com>
2025-09-19 11:13:14 +0200

    rework key

--- My project/Assets/Scripts/Manager/GameManager.cs
+++ My project/Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace Manager
@@ -68,7 +69,7 @@ namespace Manager
 
         private void Update()
         {
-            if (currentState == GameState.Menu && Input.anyKeyDown && !gameLaunched)
+            if (currentState == GameState.Menu && (Keyboard.current[Key.F].wasPressedThisFrame || Keyboard.current[Key.LeftShift].wasPressedThisFrame) && !gameLaunched)
             {
                 StartGame();
                 gameLaunched = true;
